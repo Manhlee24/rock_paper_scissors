946c724e2c6af45815df3c634ac481e4c1286c6c
Manhlee24 <lehongmanhub123@gmail.com>
2025-08-26 18:06:56 +0700

    first commit

---
+++ rock_paper_scissors.py
@@ -0,0 +1,32 @@
+import random
+
+choices= ["rock", "paper", "scissors"]
+
+print("Welcome to the Rock-Paper-Scissors game!")
+print("You'll be playing against the computer. Let's see who wins!")
+
+while True:
+    player_choice= input("Choose(rock, paper, or scissors)? Or type 'exit' to quit game: ").lower()
+    if(player_choice=="exit"):
+        print("Thank for playing. See you next time!")
+        break
+    if player_choice not in choices:
+        print("Invalid choice. Please choose 'rock', 'paper', or 'scissors'. ")
+        continue
+    computer_choice= random.choice(choices)
+    print(f"\nYour choose: {player_choice}")
+    print(f"Computer's choice: {computer_choice}\n ")
+
+    #Determined the winner
+    if player_choice==computer_choice:
+        print("It's a tie! You both chose the same")
+    elif (player_choice=="rock" and computer_choice=="scissors") or \
+         (player_choice == "paper" and computer_choice == "rock") or \
+         (player_choice == "scissors" and computer_choice == "paper"):
+        print("You won!! Congratulation!!")
+    else:
+        print("You lose!!Computer won ")
+
+
+
+
